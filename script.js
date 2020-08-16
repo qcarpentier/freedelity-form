ced906fce685948c8ca4beb6f7a27f6c856257e6
Quentin Carpentier <quentin.carpentier@outlook.be>
2020-08-17 00:08:16 +0200

    Beautiful code and add styling

--- script.js
+++ script.js
@@ -1,93 +1,75 @@
 $(".ui.form").form({
-  fields: {
-    "first-name": {
-      identifier: "first-name",
-      rules: [
-        {
-          type: "empty",
-          prompt: "Please enter your first name.",
+    fields: {
+        "first-name": {
+            identifier: "first-name",
+            rules: [{
+                type: "empty",
+                prompt: "Please enter your first name.",
+            }, ],
         },
-      ],
-    },
-    "last-name": {
-      identifier: "last-name",
-      rules: [
-        {
-          type: "empty",
-          prompt: "Please enter your last name.",
+        "last-name": {
+            identifier: "last-name",
+            rules: [{
+                type: "empty",
+                prompt: "Please enter your last name.",
+            }, ],
         },
-      ],
-    },
-    "address-street": {
-      identifier: "address-street",
-      rules: [
-        {
-          type: "empty",
-          prompt: "Please enter your current address.",
+        "address-street": {
+            identifier: "address-street",
+            rules: [{
+                type: "empty",
+                prompt: "Please enter your current address.",
+            }, ],
         },
-      ],
-    },
-    "address-number": {
-      identifier: "address-number",
-      rules: [
-        {
-          type: "empty",
-          prompt: "Please enter your address number.",
+        "address-number": {
+            identifier: "address-number",
+            rules: [{
+                type: "empty",
+                prompt: "Please enter your address number.",
+            }, ],
         },
-      ],
-    },
-    postcode: {
-      identifier: "postcode",
-      rules: [
-        {
-          type: "regExp",
-          value: /\d{4}/i,
-          prompt: "Please enter your belgian postcode.",
+        postcode: {
+            identifier: "postcode",
+            rules: [{
+                type: "regExp",
+                value: /\d{4}/i,
+                prompt: "Please enter your belgian postcode.",
+            }, ],
         },
-      ],
-    },
-    city: {
-      identifier: "city",
-      rules: [
-        {
-          type: "empty",
-          prompt: "Please enter your city.",
+        city: {
+            identifier: "city",
+            rules: [{
+                type: "empty",
+                prompt: "Please enter your city.",
+            }, ],
         },
-      ],
-    },
-    email: {
-      identifier: "email",
-      rules: [
-        {
-          type: "email",
+        email: {
+            identifier: "email",
+            rules: [{
+                type: "email",
+            }, ],
         },
-      ],
-    },
-    phone: {
-      identifier: "phone",
-      optional: true,
-      rules: [
-        {
-          type: "regExp",
-          value: /^((\+|00)32\s?|0)4(60|[789]\d)(\s?\d{2}){3}$/,
-          prompt: "Please enter a valid belgian phone number.",
+        phone: {
+            identifier: "phone",
+            optional: true,
+            rules: [{
+                type: "regExp",
+                value: /^((\+|00)32\s?|0)4(60|[789]\d)(\s?\d{2}){3}$/,
+                prompt: "Please enter a valid belgian phone number.",
+            }, ],
         },
-      ],
-    },
-    "eid-number": {
-      identifier: "eid-number",
-      optional: true,
-      rules: [
-        {
-          type: "regExp",
-          value: /^\d{3}\-\d{7}\-\d{2}$/,
-          prompt: "Please enter a valid eID number.",
+        "eid-number": {
+            identifier: "eid-number",
+            optional: true,
+            rules: [{
+                type: "regExp",
+                value: /^\d{3}\-\d{7}\-\d{2}$/,
+                prompt: "Please enter a valid eID number.",
+            }, ],
         },
-      ],
     },
-  },
 });
 
 $("form").on("submit", function (e) {
-  e.preventDefault();
+    e.preventDefault();
 });
